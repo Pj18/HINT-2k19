c939faa305b4e4b92589221a16eced2352d0fcbe
Pj18 <punit18071999@gmail.com>
2019-03-30 04:36:00 +0530

    Basic Scene
    Added the buttons for main scene and functionality to previous and next button

---
+++ HINT-2k19/Assets/scripts/dishchnge.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dishchnge : MonoBehaviour
+{
+
+    public GameObject[] myDishes;
+    public Button next;
+    public Button previous;
+    private int dishNumber = 0;
+    public int numberOfDishes=3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //myDishes = new GameObject[3];
+        for(int i=0;i<numberOfDishes;i++)
+        {
+            if(i==0)
+            {
+                myDishes[i].SetActive(true);
+            }
+            else
+            {
+                myDishes[i].SetActive(false);
+            }
+        }
+        next.onClick.AddListener(nextDish);
+        previous.onClick.AddListener(prevDish);
+    }
+
+    void nextDish()
+    {
+        dishNumber++;
+        dishNumber = dishNumber % 3;
+        for(int i=0;i<numberOfDishes;i++)
+        {
+            if(i==dishNumber)
+            {
+                myDishes[i].SetActive(true);
+            }
+            else
+            {
+                myDishes[i].SetActive(false);
+            }
+        }
+    }
+
+    void prevDish()
+    {
+        dishNumber--;
+
+        if(dishNumber<0)
+        {
+            dishNumber = numberOfDishes - 1;
+        }
+        for (int i = 0; i < numberOfDishes; i++)
+        {
+            if (i == dishNumber)
+            {
+                myDishes[i].SetActive(true);
+            }
+            else
+            {
+                myDishes[i].SetActive(false);
+            }
+        }
+    }
+}
